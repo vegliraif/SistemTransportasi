a03dff24e87b03d48d0f31ec3ce21d2f5797eecb
vegliraif <veglirafi@gmail.com>
2024-12-20 04:34:32 +0700

    Update Taxi.java

--- src/Taxi.java
+++ src/Taxi.java
@@ -1,6 +1,20 @@
 public class Taxi extends Kendaraan {
-    public Taxi(String id, int kapasitas, String statusOperasional, String namaRute) {
-        super(id, kapasitas, statusOperasional, namaRute);
+    private String titikAwal;
+    private String titikAkhir;
+
+    public Taxi(String id, int kapasitas, String statusOperasional, String namaRute, String titikAwal, String titikAkhir, double harga, String waktuKeberangkatan, String waktuTiba, String tanggal, double pendapatanHarian) {
+        super(id, kapasitas, statusOperasional, namaRute, harga, waktuKeberangkatan, waktuTiba, tanggal, pendapatanHarian);
+        this.titikAwal = titikAwal;
+        this.titikAkhir = titikAkhir;
+    }
+
+    // Getter untuk atribut tambahan
+    public String getTitikAwal() {
+        return titikAwal;
+    }
+
+    public String getTitikAkhir() {
+        return titikAkhir;
     }
 
     @Override
