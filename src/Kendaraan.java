d0f999db8cab82fff17a61917cb3c8e8c16e16e0
vegliraif <veglirafi@gmail.com>
2024-12-20 02:53:33 +0700

    Update Kendaraan.java

--- src/Kendaraan.java
+++ src/Kendaraan.java
@@ -1,4 +1,4 @@
-abstract class Kendaraan {
+public abstract class Kendaraan {
     private String id;
     private int kapasitas;
     private String statusOperasional;
@@ -15,36 +15,24 @@ abstract class Kendaraan {
     public abstract String getJenis();
 
     // Getters and setters
-    public String getId() {
-        return id;
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public int getKapasitas() { return kapasitas; }
+    public void setKapasitas(int kapasitas) { this.kapasitas = kapasitas; }
+
+    public String getStatusOperasional() { return statusOperasional; }
+    public void setStatusOperasional(String statusOperasional) { this.statusOperasional = statusOperasional; }
+
+    public String getNamaRute() { return namaRute; }
+    public void setNamaRute(String namaRute) { this.namaRute = namaRute; }
+
+    // Operasi kendaraan
+    public void mulaiOperasi() {
+        System.out.println("Kendaraan dengan ID " + id + " mulai operasi.");
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getKapasitas() {
-        return kapasitas;
-    }
-
-    public void setKapasitas(int kapasitas) {
-        this.kapasitas = kapasitas;
-    }
-
-    public String getStatusOperasional() {
-        return statusOperasional;
-    }
-
-    public void setStatusOperasional(String statusOperasional) {
-        this.statusOperasional = statusOperasional;
-    }
-
-    public String getNamaRute() {
-        return namaRute;
-    }
-
-    public void setNamaRute(String namaRute) {
-        this.namaRute = namaRute;
+    public void berhentiOperasi() {
+        System.out.println("Kendaraan dengan ID " + id + " berhenti operasi.");
     }
 }
-
