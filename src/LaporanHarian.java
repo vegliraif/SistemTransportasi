c5756800234f6dc53061e0111475df87f9d554b0
vegliraif <veglirafi@gmail.com>
2024-12-20 02:56:03 +0700

    Update LaporanHarian.java

--- src/LaporanHarian.java
+++ src/LaporanHarian.java
@@ -1,1 +1,19 @@
+public class LaporanHarian {
+    private double pendapatan;
+    private int jumlahPenumpang;
 
+    public LaporanHarian(double pendapatan, int jumlahPenumpang) {
+        this.pendapatan = pendapatan;
+        this.jumlahPenumpang = jumlahPenumpang;
+    }
+
+    public double getPendapatan() { return pendapatan; }
+    public void setPendapatan(double pendapatan) { this.pendapatan = pendapatan; }
+
+    public int getJumlahPenumpang() { return jumlahPenumpang; }
+    public void setJumlahPenumpang(int jumlahPenumpang) { this.jumlahPenumpang = jumlahPenumpang; }
+
+    public void tampilkanLaporan() {
+        System.out.println("Pendapatan: " + pendapatan + ", Jumlah Penumpang: " + jumlahPenumpang);
+    }
+}
