c9997bf223488fad744af17a921c8037ad4ac719
vegliraif <veglirafi@gmail.com>
2024-12-20 04:33:06 +0700

    Update Kendaraan.java

--- src/Kendaraan.java
+++ src/Kendaraan.java
@@ -1,38 +1,82 @@
-public abstract class Kendaraan {
+import java.io.Serializable;
+
+public abstract class Kendaraan implements Serializable {
     private String id;
     private int kapasitas;
     private String statusOperasional;
     private String namaRute;
+    private double harga;
+    private String waktuKeberangkatan;
+    private String waktuTiba;
+    private String tanggal;
+    private double pendapatanHarian;
 
-    public Kendaraan(String id, int kapasitas, String statusOperasional, String namaRute) {
+    // Constructor
+    public Kendaraan(String id, int kapasitas, String statusOperasional, String namaRute, double harga, String waktuKeberangkatan, String waktuTiba, String tanggal, double pendapatanHarian) {
         this.id = id;
         this.kapasitas = kapasitas;
         this.statusOperasional = statusOperasional;
         this.namaRute = namaRute;
+        this.harga = harga;
+        this.waktuKeberangkatan = waktuKeberangkatan;
+        this.waktuTiba = waktuTiba;
+        this.tanggal = tanggal;
+        this.pendapatanHarian = pendapatanHarian;
     }
 
-    // Abstract method
+    // Getter dan Setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getKapasitas() {
+        return kapasitas;
+    }
+
+    public void setKapasitas(int kapasitas) {
+        this.kapasitas = kapasitas;
+    }
+
+    public String getStatusOperasional() {
+        return statusOperasional;
+    }
+
+    public void setStatusOperasional(String statusOperasional) {
+        this.statusOperasional = statusOperasional;
+    }
+
+    public String getNamaRute() {
+        return namaRute;
+    }
+
+    public void setNamaRute(String namaRute) {
+        this.namaRute = namaRute;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public String getWaktuKeberangkatan() {
+        return waktuKeberangkatan;
+    }
+
+    public String getWaktuTiba() {
+        return waktuTiba;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public double getPendapatanHarian() {
+        return pendapatanHarian;
+    }
+
+    // Metode abstract untuk jenis kendaraan
     public abstract String getJenis();
-
-    // Getters and setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-
-    public int getKapasitas() { return kapasitas; }
-    public void setKapasitas(int kapasitas) { this.kapasitas = kapasitas; }
-
-    public String getStatusOperasional() { return statusOperasional; }
-    public void setStatusOperasional(String statusOperasional) { this.statusOperasional = statusOperasional; }
-
-    public String getNamaRute() { return namaRute; }
-    public void setNamaRute(String namaRute) { this.namaRute = namaRute; }
-
-    // Operasi kendaraan
-    public void mulaiOperasi() {
-        System.out.println("Kendaraan dengan ID " + id + " mulai operasi.");
-    }
-
-    public void berhentiOperasi() {
-        System.out.println("Kendaraan dengan ID " + id + " berhenti operasi.");
-    }
 }
