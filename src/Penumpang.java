2652909aae323336796bf5160a27ce10dff66d1d
vegliraif <veglirafi@gmail.com>
2024-12-20 02:59:04 +0700

    Update Penumpang.java

--- src/Penumpang.java
+++ src/Penumpang.java
@@ -1,1 +1,16 @@
+public class Penumpang {
+    private String nama;
+    private String idPenumpang;
+
+    public Penumpang(String nama, String idPenumpang) {
+        this.nama = nama;
+        this.idPenumpang = idPenumpang;
+    }
+
+    public String getNama() { return nama; }
+    public void setNama(String nama) { this.nama = nama; }
+
+    public String getIdPenumpang() { return idPenumpang; }
+    public void setIdPenumpang(String idPenumpang) { this.idPenumpang = idPenumpang; }
+}
 
