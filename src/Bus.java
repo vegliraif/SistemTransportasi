b03117c5368c8d4f0f84d3279c53ec8758b27ce9
vegliraif <veglirafi@gmail.com>
2024-12-20 04:32:41 +0700

    Update Bus.java

--- src/Bus.java
+++ src/Bus.java
@@ -1,6 +1,26 @@
-class Bus extends Kendaraan {
-    public Bus(String id, int kapasitas, String statusOperasional, String namaRute) {
-        super(id, kapasitas, statusOperasional, namaRute);
+public class Bus extends Kendaraan {
+    private String titikAwal;
+    private String titikAkhir;
+    private String daftarPemberhentian;
+
+    public Bus(String id, int kapasitas, String statusOperasional, String namaRute, String titikAwal, String titikAkhir, String daftarPemberhentian, double harga, String waktuKeberangkatan, String waktuTiba, String tanggal, double pendapatanHarian) {
+        super(id, kapasitas, statusOperasional, namaRute, harga, waktuKeberangkatan, waktuTiba, tanggal, pendapatanHarian);
+        this.titikAwal = titikAwal;
+        this.titikAkhir = titikAkhir;
+        this.daftarPemberhentian = daftarPemberhentian;
+    }
+
+    // Getter untuk atribut tambahan
+    public String getTitikAwal() {
+        return titikAwal;
+    }
+
+    public String getTitikAkhir() {
+        return titikAkhir;
+    }
+
+    public String getDaftarPemberhentian() {
+        return daftarPemberhentian;
     }
 
     @Override
